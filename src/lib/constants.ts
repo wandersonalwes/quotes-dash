2ee083d5be480e64f556451a898072ed60a4f69a
wandersonalwes <wandersonalwes@hotmail.com>
2021-01-14 11:15:10 -0300

    chore: change strategy constant API_URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.API_URL || 'http://localhost:3000/api'
+export const API_URL = process.env.NODE_ENV === 'production' ? 'https://quotes.wandersonalwes.com.br/api' : 'http://localhost:3000/api'
