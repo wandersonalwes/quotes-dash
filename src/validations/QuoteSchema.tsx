66085efcd9c05993cc04232081bcc206b3979cca
wandersonalwes <wandersonalwes@hotmail.com>
2021-01-14 17:25:17 -0300

    chore: add correct validation in QuoteSchema

--- src/validations/QuoteSchema.tsx
+++ src/validations/QuoteSchema.tsx
@@ -4,7 +4,7 @@ const QuoteSchema = Yup.object().shape({
   content: Yup.string()
     .min(10, 'Este campo deve ter pelo menos 10 caracteres')
     .required('Campo obrigatório'),
-  categories: Yup.array().min(1, 'Você deve selecionar pelo menos 1 categoria')
+  selectedCategories: Yup.array().min(1, 'Você deve selecionar pelo menos 1 categoria')
 })
 
 export default QuoteSchema
