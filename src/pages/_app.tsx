d17d0b44bbb532c241a37a930593c4d81130ee57
wandersonalwes <wandersonalwes@hotmail.com>
2021-01-14 19:04:08 -0300

    chore: add react-toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import LeftMenuProvider from '@/contexts/LeftMenu'
 import { Provider } from 'next-auth/client'
 import { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +11,7 @@ function MyApp ({ Component, pageProps }: AppProps) {
       <LeftMenuProvider>
         <Component {...pageProps} />
       </LeftMenuProvider>
+      <ToastContainer />
     </Provider>
   )
 }
