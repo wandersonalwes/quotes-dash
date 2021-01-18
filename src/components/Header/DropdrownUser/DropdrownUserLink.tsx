b6ae3b4b645d388b7c55a7efd3c6c5fe65f53779
wandersonalwes <wandersonalwes@hotmail.com>
2021-01-18 15:25:19 -0300

    chore: add correct typing in DropdownUserLink

--- src/components/Header/DropdrownUser/DropdrownUserLink.tsx
+++ src/components/Header/DropdrownUser/DropdrownUserLink.tsx
@@ -1,12 +1,17 @@
-import { FC, LinkHTMLAttributes } from 'react'
+import { AnchorHTMLAttributes, FC } from 'react'
 
-interface DropdrownUserLinkProps extends LinkHTMLAttributes<HTMLLinkElement> {
+interface DropdrownUserLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string
 }
 
 const DropdrownUserLink: FC<DropdrownUserLinkProps> = ({ href, children, ...rest }) => {
   return (
-    <a href={href} className="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white" {...rest}>{children}</a>
+    <a
+      href={href}
+      className="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white"
+      {...rest}>
+      {children}
+    </a>
   )
 }
 
