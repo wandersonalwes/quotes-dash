294e541367a64b676c1de06f4f1029ffbccb8d74
wandersonalwes <wandersonalwes@hotmail.com>
2021-01-18 15:43:47 -0300

    chore: change return random quote

--- src/pages/api/quotes/random.tsx
+++ src/pages/api/quotes/random.tsx
@@ -4,7 +4,7 @@ import prisma from '../../../lib/prisma'
 export default async function handle (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     const random = await prisma.$queryRaw('SELECT * FROM quotes ORDER BY random() LIMIT 1;')
-    return res.json(random)
+    return res.json(random[0])
   } else {
     throw new Error(
       `The HTTP ${req.method} method is not supported at this route.`
