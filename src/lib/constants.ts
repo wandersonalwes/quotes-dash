1e0481f6872afdcc4c87902c28481179cb921100
wandersonalwes <wandersonalwes@hotmail.com>
2021-01-26 15:25:55 -0300

    chore: change api url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? 'https://quotes.wandersonalwes.com.br/api' : 'http://localhost:3000/api'
+export const API_URL = process.env.NODE_ENV === 'production' ? 'https://v1.frasesparastatus.app.br/api' : 'http://localhost:3000/api'
